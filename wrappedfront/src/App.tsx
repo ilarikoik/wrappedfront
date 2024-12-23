c27bba5d94c857c2cd21ffaef3ddc9128d05bed0
ilarikoik <ilari.koik@gmail.com>
2024-12-23 14:12:36 +0200

    statsarea

--- wrappedfront/src/App.tsx
+++ wrappedfront/src/App.tsx
@@ -1,9 +1,10 @@
 import Dropzone from "./components/Dropzone";
+import Stats from "./components/Stats";
 
 export default function App() {
   return (
     <>
-      <div className=" flex flex-col justify-center items-center h-screen bg-black p-1">
+      <div className=" flex flex-col justify-center items-center min-h-screen bg-black p-1 pb-20">
         <div className="h-fit w-fit flex flex-col justify-center items-center  mb-10 ">
           <h1 className="font-bold text-[80px] bg-gradient-to-r from-green to-gray bg-clip-text text-transparent">
             Wrapped But Better
@@ -13,8 +14,9 @@ export default function App() {
           </p>
         </div>
 
-        <div className="max-w-[1400px] max-h-[900px] w-[1200px] h-[700px] bg-dark rounded-lg">
+        <div className="max-w-[1000px] max-h-[900px] w-full h-full bg-dark  border-2 border-green rounded-xl">
           <Dropzone />
+          <Stats></Stats>
         </div>
       </div>
     </>
---
+++ wrappedfront/src/components/Stats.tsx
@@ -0,0 +1,16 @@
+export default function Stats() {
+  return (
+    <div className=" h-fit w-full ">
+      <div className="grid grid-cols-2 ">
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+        <div className="border-2 border-gray rounded-xl m-2 h-32"></div>
+      </div>
+    </div>
+  );
+}
